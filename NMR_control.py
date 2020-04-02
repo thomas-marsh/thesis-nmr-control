cb53f936e72506b9323e07b93b61988bfb8bcaaa
Thomas Marsh <tmarsh@hamilton.edu>
2020-04-02 14:57:28 -0400

    Update Button Names

--- NMR_control.py
+++ NMR_control.py
@@ -10,11 +10,19 @@ Created on Thu Apr  2 12:23:03 2020
 from PyQt5 import QtWidgets, uic
 import sys
 
+
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi('Igor.ui', self)
+        self.activate_buttons()
         self.show()
+    
+    def activate_buttons(self):
+        self.run_nifid_button.clicked.connect(self.run_nifid)
+        
+    def run_nifid(self):
+        print("test")
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
